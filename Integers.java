be7a2089db882b5d6f9ccab54365e0fe02cb76c1
AksSin2003 <akshitsinha@ymail.com>
2016-06-14 15:32:31 +0000

    Integers
    Assignment 1 of session 2

---
+++ Integers.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+/**
+ * @author Akshit
+ *
+ */
+public class Integers {
+
+	/**
+	 * @param args create 2 int with values
+	 */
+	public static void main(String[] args) {
+		
+		int num1 = 1 ;
+		float num2 =  (float) 1.5;
+		int sum =  (int) (num1 + num2);
+		
+		
+		// TODO Auto-generated method stub
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(sum);
+		
+				
+		
+	}
+
+}
